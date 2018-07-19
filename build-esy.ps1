802069945bd04a7c27d1d456b1f09ffe2ab101a3
Bryan Phelps <bryphe@outlook.com>
2018-07-19 15:05:14 -0700

    Add setup / build scripts to get bootstrapped esy executable for windows

---
+++ build-esy.ps1
@@ -0,0 +1,6 @@
+
+cd C:/projects/esy
+
+npm run bootstrap:build
+
+C:/projects/esy/_release/_build/default/esy/bin/esyCommand.exe --help
---
+++ setup-esy.ps1
@@ -0,0 +1,16 @@
+npm install -g rimraf
+npm install -g yarn
+git clone https://github.com/esy/esy C:/projects/esy
+
+cd C:/projects/esy
+git submodule update --init --recursive
+
+npm install esy-bash
+
+npm install esy-ocaml/FastReplaceString.git#9450b6
+node scripts/bootstrap/copy-fastreplacestring.js
+
+appveyor-retry yarn run bootstrap:esy-install
+
+npm run bootstrap install-opam
+npm run bootstrap install-dependencies
