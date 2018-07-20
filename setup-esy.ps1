83af17fa30b302f93d692daa32b8d121e2c4fbdc
Bryan Phelps <bryphe@outlook.com>
2018-07-20 16:13:55 -0700

    Pick up more logging

--- setup-esy.ps1
+++ setup-esy.ps1
@@ -3,7 +3,7 @@ npm install -g yarn
 git clone https://github.com/esy/esy C:/projects/esy
 
 cd C:/projects/esy
-git checkout 407d702f44770cbcde2bbcf146bc10fbad27e0c0
+git checkout 62665412ed1b182ed15e2e06a1216d374b4fdfcc
 git submodule update --init --recursive
 
 npm install esy-bash
